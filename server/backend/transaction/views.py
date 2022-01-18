262230e7eb350a0e7b024560ea58dc7b6e86acf7
rubinstory <rubinstory@naver.com>
2022-01-18 05:32:20 +0000

    django modified

--- server/backend/transaction/views.py
+++ server/backend/transaction/views.py
@@ -7,12 +7,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
+    queryset = Transaction.objects.all().order_by('-id')
     serializer_class = TransactionSerializer
-    # filter_backends = [filters.SearchFilter , DjangoFilterBackend]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user']
-    # search_fields = ['user__username']
 
 
 
